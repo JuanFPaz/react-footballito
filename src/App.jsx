1f6461171b49ac6dbbe473d24914687bbc6249a9
Juan Paz <jpaz.dev94@gmail.com>
2024-04-14 13:42:24 -0300

    commit previo a ramificacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+// function App () {
+//   // Bueno, hacer la primera conexion
+//   const [data, setData] = useState(null)
+//   useEffect(() => {
+//     fetch('http://localhost:3000')
+//       .then((res) => {
+//         return res.json()
+//       })
+//       .then(data => {
+//         setData(data)
+//         console.log(data)
+//       })
+//       .catch(err => {
+//         console.error(err)
+//         setData({ mensaje: err.message })
+//       })
+//   }, [])
+//   return (
+//     <>
+//       {data && (<h1>{data.mensaje} :3</h1>)}
+//     </>
+//   )
+// }
+
 function App () {
-  // Bueno, hacer la primera conexion
-  const [data, setData] = useState(null)
-  useEffect(() => {
-    fetch('http://localhost:3000')
-      .then((res) => {
-        return res.json()
-      })
-      .then(data => {
-        setData(data)
-        console.log(data)
-      })
-      .catch(err => {
-        console.error(err)
-        setData({ mensaje: err.message })
-      })
-  }, [])
   return (
     <>
-      {data && (<h1>{data.mensaje} :3</h1>)}
     </>
   )
 }
