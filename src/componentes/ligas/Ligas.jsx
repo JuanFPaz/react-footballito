5b7e728f4bf06d0018358114d4d67f3852db1895
Juan Paz <jpaz.dev94@gmail.com>
2024-04-17 20:15:03 -0300

    Agrege dos nuevos estados, para recibir la props desestructurada, y obtener el nobmre de la liga, y sus temporadas, cuando es seleccioanda por el navside de Principal. Ademas agregue un useEffect para controlar cuando es eselccionado otro item de la navside, y cambian los valores de sus props.

--- src/componentes/ligas/Ligas.jsx
+++ src/componentes/ligas/Ligas.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-lone-blocks */
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react'
 
@@ -10,8 +11,7 @@ function Fixture () {
 }
 
 /* TODO: ARREGLA ESTA PROP POR EL AMOR DE DIOS, osea la props esta bien, pero no entiendo porque me hice tanto lio, mañana la veo con más calma */
-function Tabla ({ tablaData }) {
-  console.log(tablaData)
+function Tabla () {
   return (
     <>
       <table border='1'>
@@ -52,27 +52,72 @@ function Tabla ({ tablaData }) {
   )
 }
 
-function Ligas () {
+/* TOODO: arreglar el dataTable, anda, pero la desestructuracion esta medio tomada de los pelos. */
+function Ligas ({ league, seasons }) {
   const [loading, setLoading] = useState(true)
-  const [dataTable, setDataTable] = useState(null)
+  const [dataStadistic, setDataStadistic] = useState(null)
+  const [dataLeague, setDataLeague] = useState({})
+  const [dataSeasons, setDataSeasons] = useState([])
+
   useEffect(() => {
+    { /*
+
+      Ahora con los estaods dataLeague y dataSeason, podrimaos crear un fetch dinamico del endpoint
+        Por ejemplo:
+        localhost:3000/2024/ligra-profesional-argentina -> Cuando es seleccionada la liga
+        localhost:3000/2024/copa-de-la-liga-profesiona -> Cuando es seleccionada la copa de la ligarcha
+
+    */ }
     fetch('http://localhost:3000/2024/liga-profesional-argentina')
       .then(res => res.json())
       .then(data => {
-        const { response: [{ standing }] } = data
+        const { response } = data
         setLoading(false)
-        setDataTable(standing)
+        setDataStadistic(response)
       })
   }, [])
+
+  useEffect(() => {
+    setDataLeague({ ...league })
+    setDataSeasons([...seasons])
+  }, [league, seasons])
   return (
     <>
-      {loading && <h1>cargando</h1>}
-      {dataTable && (
-        <>
-          <Tabla tablaData={dataTable} />
-          <Fixture />
-        </>
-      )}
+      {/* ESTOS DATOS LOS PODEMOS OBTENER DEL PRIMER GET (APP -> PRINCIPAL -> NAVASIDE)
+      - Cuando seleccinamos un item de la lista, ademas del renderizado condicional, podemos enviar a los componentes seleccionados, los datos como el nombre de la liga y lase seasons disponibles a mostrar.  */}
+      <header>
+        <h1>{dataLeague.name}</h1>
+        <nav>
+          <h2>Temporadas:</h2>
+          <ul>
+            {dataSeasons.map(ds =>
+              (
+                <li key={ds.year}>
+                  Temporada {ds.year}
+                </li>
+              )
+            )}
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <h3>
+          Equipos :
+        </h3>
+        <ul>
+          <li>RiBer</li>
+        </ul>
+      </section>
+      <section>
+        {loading && <h1>cargando</h1>}
+        {dataStadistic && (
+          <>
+            {/* <Tabla tablaData={dataStadistic} /> */}
+            <Fixture />
+          </>
+        )}
+
+      </section>
 
     </>
   )
