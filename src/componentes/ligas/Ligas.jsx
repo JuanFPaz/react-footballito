bb779394539c0ad66b5aca14d44759c3b206e706
Juan Paz <jpaz.dev94@gmail.com>
2024-04-18 01:17:55 -0300

    Me olvide de hacer el merge -_-'

--- src/componentes/ligas/Ligas.jsx
+++ src/componentes/ligas/Ligas.jsx
@@ -11,70 +11,79 @@ function Fixture () {
 }
 
 /* TODO: ARREGLA ESTA PROP POR EL AMOR DE DIOS, osea la props esta bien, pero no entiendo porque me hice tanto lio, mañana la veo con más calma */
-function Tabla () {
-  return (
-    <>
-      <table border='1'>
-        <thead>
-          <tr>
-            <th>Ranking</th>
-            <th>Equipo</th>
-            <th>Puntos</th>
-            <th>PJ</th>
-            <th>PG</th>
-            <th>PE</th>
-            <th>PP</th>
-            <th>Dif Gol</th>
-            <th>Goles favor</th>
-            <th>Goles encontra</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tablaData.map((equipo, idx) =>
-            (
-              <tr key={equipo.team.id}>
-                <td>{idx + 1}</td>
-                <td>{equipo.team.name}</td>
-                <td>{equipo.points}</td>
-                <td>{equipo.all.played}</td>
-                <td>{equipo.all.win}</td>
-                <td>{equipo.all.draw}</td>
-                <td>{equipo.all.lose}</td>
-                <td>{equipo.goalsDiff}</td>
-                <td>{equipo.all.goals.for}</td>
-                <td>{equipo.all.goals.against}</td>
-              </tr>
-            )
-          )}
-        </tbody>
-      </table>
-    </>
-  )
+function Tabla ({ response }) {
+  console.log(response)
+  // return (
+  //   <>
+  //     <table border='1'>
+  //       <thead>
+  //         <tr>
+  //           <th>Ranking</th>
+  //           <th>Equipo</th>
+  //           <th>Puntos</th>
+  //           <th>PJ</th>
+  //           <th>PG</th>
+  //           <th>PE</th>
+  //           <th>PP</th>
+  //           <th>Dif Gol</th>
+  //           <th>Goles favor</th>
+  //           <th>Goles encontra</th>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //         {tablaData.map((equipo, idx) =>
+  //           (
+  //             <tr key={equipo.team.id}>
+  //               <td>{idx + 1}</td>
+  //               <td>{equipo.team.name}</td>
+  //               <td>{equipo.points}</td>
+  //               <td>{equipo.all.played}</td>
+  //               <td>{equipo.all.win}</td>
+  //               <td>{equipo.all.draw}</td>
+  //               <td>{equipo.all.lose}</td>
+  //               <td>{equipo.goalsDiff}</td>
+  //               <td>{equipo.all.goals.for}</td>
+  //               <td>{equipo.all.goals.against}</td>
+  //             </tr>
+  //           )
+  //         )}
+  //       </tbody>
+  //     </table>
+  //   </>
+  // )
 }
 
 /* TOODO: arreglar el dataTable, anda, pero la desestructuracion esta medio tomada de los pelos. */
 function Ligas ({ league, seasons }) {
   const [loading, setLoading] = useState(true)
+  const [renderStadistic, setRenderStadistic] = useState(false)
   const [dataStadistic, setDataStadistic] = useState(null)
   const [dataLeague, setDataLeague] = useState({})
   const [dataSeasons, setDataSeasons] = useState([])
 
   useEffect(() => {
-    { /*
+    /*
 
-      Ahora con los estaods dataLeague y dataSeason, podrimaos crear un fetch dinamico del endpoint
-        Por ejemplo:
-        localhost:3000/2024/ligra-profesional-argentina -> Cuando es seleccionada la liga
-        localhost:3000/2024/copa-de-la-liga-profesiona -> Cuando es seleccionada la copa de la ligarcha
+    Ahora con los estaods dataLeague y dataSeason, podrimaos crear un fetch dinamico del endpoint
+      Por ejemplo:
+      localhost:3000/2024/ligra-profesional-argentina -> Cuando es seleccionada la liga
+      localhost:3000/2024/copa-de-la-liga-profesiona -> Cuando es seleccionada la copa de la ligarcha
 
-    */ }
-    fetch('http://localhost:3000/2024/liga-profesional-argentina')
+  */
+    const season = '2024'
+    const league = 'liga-profesional-argentina'
+    console.log('LOG DE USEFFECT 1')
+    console.log(dataLeague)
+    console.log(dataSeasons)
+    fetch(`http://localhost:3000/${season}/${league}`)
       .then(res => res.json())
       .then(data => {
-        const { response } = data
         setLoading(false)
-        setDataStadistic(response)
+        setRenderStadistic(true)
+        setDataStadistic([...data])
+        console.log(data)
       })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useEffect(() => {
@@ -110,9 +119,9 @@ function Ligas ({ league, seasons }) {
       </section>
       <section>
         {loading && <h1>cargando</h1>}
-        {dataStadistic && (
+        {renderStadistic && (
           <>
-            {/* <Tabla tablaData={dataStadistic} /> */}
+            <Tabla tablaData={dataStadistic} />
             <Fixture />
           </>
         )}
