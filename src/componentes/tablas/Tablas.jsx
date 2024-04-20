4387baa9062bf591c91af7425e92b6e55a019817
Juan Paz <jpaz.dev94@gmail.com>
2024-04-20 11:50:47 -0300

    Primera componente reciclabe (por asi decirlo), de la app, el componente tabla y su contenedor

---
+++ src/componentes/tablas/Tablas.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+function TablaContainer ({ tablaItems }) {
+  return (
+    <>
+      <table border='1'>
+        <thead>
+          <tr>
+            <th>Ranking</th>
+            <th>Equipo</th>
+            <th>Puntos</th>
+            <th>PJ</th>
+            <th>PG</th>
+            <th>PE</th>
+            <th>PP</th>
+            <th>Dif Gol</th>
+            <th>Goles favor</th>
+            <th>Goles encontra</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tablaItems.map(({ team: { id, name }, points, goalsDiff, all: { played, win, draw, lose, goals: { for: aFavor, against } } }, idx) => (
+            <tr key={id}>
+              <td>{idx + 1}</td>
+              <td>{name}</td>
+              <td>{points}</td>
+              <td>{played}</td>
+              <td>{win}</td>
+              <td>{draw}</td>
+              <td>{lose}</td>
+              <td>{goalsDiff}</td>
+              <td>{aFavor}</td>
+              <td>{against}</td>
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default function Tabla ({ response: [{ standings }] }) {
+  const [dataStanding, setDataStanding] = useState([...standings])
+
+  return (
+    <>
+      {dataStanding.map((standing, idx) => (
+        <TablaContainer key={idx} tablaItems={standing} />
+      ))}
+    </>
+  )
+}
